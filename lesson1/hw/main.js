3da49bc8f88624a362577c977598a290f8be9ec5
Pavlo1994Dokin <dokin.pavlo.1994@gmail.com>
2022-02-01 15:14:53 +0200

    stars hw_lesson1

---
+++ lesson1/hw/main.js
@@ -0,0 +1,22 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+let txt = 'Hello';
+console.log(txt);
+alert(txt);
+document.write(`<div>${txt}</div>`);
+
+let xxx = 'owu';
+console.log(xxx);
+alert(xxx);
+document.write('owu');
+
+let zxc = 'com';
+console.log(zxc);
+alert(zxc);
+document.write(`<div>${zxc}</div>`);
+
+let asd = 'ua';
+console.log(asd);
+alert(asd);
+document.write('ua');
