23b51a0d90849095d75bcf4c6653fd9e197a65e2
Pavlo1994Dokin <dokin.pavlo.1994@gmail.com>
2022-02-02 00:41:33 +0200

    errors_lesson1

---
+++ lesson1/errors/main.js
@@ -0,0 +1,25 @@
+// Ваше завдання, зробити так, щоб файл відкривався без помилок .
+
+let a = 100;
+console.log(a);
+
+let b = 500;
+console.log(b);
+
+let c = "hello";
+console.log(c);
+
+let d = "okten";
+console.log(d);
+
+let x = 'constant value';
+console.log(x);
+
+x = 'new value';
+console.log(x);
+
+b = 'new value';
+console.log(b);
+
+let result = 100 + b;
+console.log(result);
