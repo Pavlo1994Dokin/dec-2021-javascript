34aadd64d806d2d35d85f48c3f691129622cf5e3
Pavlo1994Dokin <dokin.pavlo.1994@gmail.com>
2022-02-08 16:21:21 +0200

    start_hw_lesson4

---
+++ lesson4/hw/main.js
@@ -0,0 +1,35 @@
+//- створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+// function calc(a, b) {
+//     return a * b;
+// }
+//
+// console.log(calc(4, 5));
+
+
+//- створити функцію яка обчислює та повертає площу кола з радіусом r
+
+// function cirecle() {
+//     let r = 4 * 2;
+//     let pCirecle = 3.14 * r;
+//     return pCirecle;
+// }
+//
+// console.log(cirecle());
+// document.write('Площа кола :' + cirecle());
+
+
+
+//- створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r S=2pRH
+
+// function cylinder (r, h){
+//     let sCylinder = 2*3.14*r*h;
+//     return sCylinder;
+// }
+// console.log(cylinder(5,  12));
+
+
+
+//- створити функцію яка приймає масив та виводить кожен його елемент
+
+
