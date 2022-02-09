9a2a3ebd4ac826fb5329bb94a781c66348687495
Pavlo1994Dokin <dokin.pavlo.1994@gmail.com>
2022-02-10 00:30:32 +0200

    finish_hw_lesson4

--- lesson4/hw/main.js
+++ lesson4/hw/main.js
@@ -18,6 +18,12 @@
 // console.log(cirecle());
 // document.write('Площа кола :' + cirecle());
 
+// function circle(p, r) {
+//     r = r * r;
+//     return p * r;
+// }
+// console.log(circle(3.14, 4));
+
 
 //- створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r S=2pRH
 
@@ -26,6 +32,12 @@
 //     return sCylinder;
 // }
 // console.log(cylinder(5,  12));
+
+// function culinder(h, r) {
+//     let p = 3.14;
+//     return 2 * p * h * r;
+// }
+// console.log(culinder(10, 5));
 
 
 //- створити функцію яка приймає масив та виводить кожен його елемент
@@ -38,6 +50,14 @@
 // }
 // allArray(array);
 
+// let array=[1,2,3, 'hi', 'okten', 'paha']
+// function SomeArray(array) {
+//     for (let arrayElement of array) {
+//         console.log(arrayElement);
+//     }
+// }
+// SomeArray(array)
+
 
 //- створити функцію яка створює параграф з текстом. Текст задати через аргумент
 
@@ -45,6 +65,11 @@
 //     document.write(`<p>${text}</p>`);
 // }
 // txt('hello');
+
+// function Paragraf(txt){
+//     document.write(`<p>${txt}</p>`);
+// }
+// Paragraf('Hello Okten');
 
 
 //- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
@@ -59,6 +84,15 @@
 //
 // listCreated('hello')
 
+// function SomeTxt(text){
+//     document.write('<ul>');
+//     document.write(`<li>${text}</li>`);
+//     document.write(`<li>${text}</li>`);
+//     document.write(`<li>${text}</li>`);
+//     document.write('</ul>');
+// }
+// SomeTxt('Hi Okten school');
+
 
 //- створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
@@ -72,6 +106,17 @@
 // }
 //
 // listCreated('hello', 3);
+
+// function someArray(txt, nmb) {
+//     document.write('<ul>');
+//     for (let i = 0; i < nmb; i++) {
+//         document.write(`<li>${txt}</li>`);
+//
+//     }
+//     document.write('</ul>');
+// }
+//
+// someArray('hi', 5);
 
 
 //- створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
@@ -88,6 +133,18 @@
 //
 // any(nmb);
 
+// let elements = [11, 22, 33, 'hi', 'okten', 'school', true, false];
+//
+// function SomeArray(elements) {
+//     document.write('<ul>');
+//     for (let i = 0; i < elements.length; i++) {
+//         document.write(`<li>${elements[i]}</li>`);
+//     }
+//     document.write('</ul>');
+// }
+//
+// SomeArray(elements);
+
 
 //- створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
 
@@ -100,3 +157,13 @@
 // }
 //
 // some(last);
+
+// let last = [{id: 1, name: 'Hello', age: 21}, {id: 2, name: 'Okten', age: 32}, {id: 3, name: 'school', age: 22}];
+//
+// function SomeArray(last) {
+//     for (element of last) {
+//         document.write(`<div>${element.id}. ${element.name} - ${element.age}</div>`);
+//     }
+// }
+//
+// SomeArray(last)
