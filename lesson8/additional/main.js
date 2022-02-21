fa928be6ea87a2c03122631f905d25e554d7cad3
Pavlo1994Dokin <dokin.pavlo.1994@gmail.com>
2022-02-21 15:30:26 +0200

    lesson8 additional

--- lesson8/additional/main.js
+++ lesson8/additional/main.js
@@ -1,11 +1,14 @@
 //написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
-//
 
-
-let arr = []
-for (const arrayElement of array) {
-    let aa = arrayElement.split(' ')
-    for (const aaElement of aa) {
-        arr.push(aaElement)
+let classes=[]
+console.log(classes);
+for (const child of children) {
+if (child.classList.length) {
+    for (const childElement of child.classList) {
+        if (classes.indexOf(childElement) === -1) {
+            classes.push(childElement);
+        }
     }
+}
+collectClasses(child);
 }
